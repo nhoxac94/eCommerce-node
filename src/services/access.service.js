02606d1613ed5b778218ac3007f5a5276eea979b
Le Hoang Loi <nhoxac94@yahoo.com>
2023-10-18 22:27:06 +0700

    date 3 - add middle ware handle error

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -4,6 +4,7 @@ const crypto = require("crypto");
 const KeyTokenService = require("./keyToken.service");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const { BadRequestError } = require("../core/error.response");
 
 const roleShop = {
   SHOP: "SHOP",
@@ -14,88 +15,74 @@ const roleShop = {
 
 class AccessService {
   static signUp = async ({ name, email, password }) => {
-    try {
-      // step1: check valid email
-      const holderShop = await shopModel.findOne({ email }).lean();
+    // step1: check valid email
+    const holderShop = await shopModel.findOne({ email }).lean();
 
-      if (holderShop) {
-        return {
-          code: "xxx",
-          message: "Shop already registered!",
-        };
-      }
+    if (holderShop) {
+      throw new BadRequestError("Error: Shop already registered!");
+    }
 
-      const passwordHash = await bcrypt.hash(password, 10);
+    const passwordHash = await bcrypt.hash(password, 10);
 
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password: passwordHash,
-        roles: [roleShop.SHOP],
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password: passwordHash,
+      roles: [roleShop.SHOP],
+    });
+
+    if (newShop) {
+      // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      //   modulusLength: 4096,
+      //   publicKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      //   privateKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      // });
+
+      const privateKey = crypto.randomBytes(64).toString("hex");
+      const publicKey = crypto.randomBytes(64).toString("hex");
+
+      console.log({ privateKey, publicKey });
+
+      const keyStore = await KeyTokenService.createKeyToken({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
       });
 
-      if (newShop) {
-        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-        //   modulusLength: 4096,
-        //   publicKeyEncoding: {
-        //     type: "pkcs1",
-        //     format: "pem",
-        //   },
-        //   privateKeyEncoding: {
-        //     type: "pkcs1",
-        //     format: "pem",
-        //   },
-        // });
-
-        const privateKey = crypto.randomBytes(64).toString('hex')
-        const publicKey = crypto.randomBytes(64).toString('hex')
-
-        console.log({ privateKey, publicKey });
-
-        const keyStore = await KeyTokenService.createKeyToken({
-          userId: newShop._id,
-          publicKey,
-          privateKey
-        });
-
-        if (!keyStore) {
-          return {
-            code: "xxx",
-            message: "tokens error",
-          };
-        }
-
-        const tokens = await createTokenPair(
-          { userId: newShop._id, email },
-          publicKey,
-          privateKey
-        );
-
-        console.log("Create Token Success", tokens);
-
-        return {
-          code: 201,
-          metadata: {
-            shop: getInfoData({
-              fields: ["_id", "name", "email"],
-              object: newShop,
-            }),
-            tokens,
-          },
-        };
+      if (!keyStore) {
+        throw new BadRequestError("Error: Tokens error!");
       }
 
+      const tokens = await createTokenPair(
+        { userId: newShop._id, email },
+        publicKey,
+        privateKey
+      );
+
+      console.log("Create Token Success", tokens);
+
       return {
-        code: 200,
-        metadata: null,
-      };
-    } catch (error) {
-      return {
-        code: "error",
-        message: error.message,
-        status: "error",
+        code: 201,
+        metadata: {
+          shop: getInfoData({
+            fields: ["_id", "name", "email"],
+            object: newShop,
+          }),
+          tokens,
+        },
       };
     }
+
+    return {
+      code: 200,
+      metadata: null,
+    };
   };
 }
 
